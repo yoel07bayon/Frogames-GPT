a70a62f39c2779e1c08ffcca8d0c041c2cc6226c
cnoe462 <cnoe462@gmail.com>
2026-02-18 15:18:23 +0000

    Conectadodo con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:personal_ia/app/app.dart';
 import 'package:personal_ia/core/provider/chat_provider.dart';
 import 'package:personal_ia/core/provider/theme_provider.dart';
+import 'package:personal_ia/firebase_options.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //await Firebase.initializeApp(options: defaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
